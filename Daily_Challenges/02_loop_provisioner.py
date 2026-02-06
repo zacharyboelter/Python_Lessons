a110db398c4c9622e852254719a6c42270cd7288
Zachary Boelter <zboelter@gmail.com>
2026-02-06 11:32:13 -0800

    for loop template

---
+++ Daily_Challenges/02_loop_provisioner.py
@@ -0,0 +1,10 @@
+
+names = ["John", "Jane", "Doe", "Smith", "Alice", "Bob", "Charlie", "Zachary", "David", "Eve", "Frank"]
+
+for name in names:
+    if name == "Zachary":
+        print(f"Hey {name}! That's my name!")
+    else:
+        print(f'Hello {name}, come join the party!')
+
+
