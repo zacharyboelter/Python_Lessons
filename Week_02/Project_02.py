97500073026d422f31c8117531bc90b4ccaeb29e
Zachary Boelter <zboelter@gmail.com>
2026-02-13 15:08:42 -0800

    round total sales

--- Week_02/Project_02.py
+++ Week_02/Project_02.py
@@ -1,7 +1,7 @@
 # Project 2
 # Zachary Boelter
 
-# 1.Cell
+# 1.
 print('Sales Calculating Program \n \n')
 # 2.
 item_description = input('Enter item description: ')
@@ -12,4 +12,4 @@ item_price = float(input(f'Enter price per {item_description}: '))
 # 5.
 total_sales = item_number * item_price
 # 6.
-print(f'\n \nYour total order for {item_number} {item_description} is ${total_sales} at ${item_price} each.')
+print(f'\n \nYour total order for {item_number} {item_description} is ${total_sales:.2f} at ${item_price} each.')
