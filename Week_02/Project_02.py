39c32ec559addce5a9447a95ad5f8eb5d089600f
Zachary Boelter <zboelter@gmail.com>
2026-02-13 15:01:09 -0800

    first draft

--- Week_02/Project_02.py
+++ Week_02/Project_02.py
@@ -1,3 +1,15 @@
 # Project 2
 # Zachary Boelter
 
+# 1.Cell
+print('Sales Calculating Program \n \n')
+# 2.
+item_description = input('Enter item description: ')
+# 3. 
+item_number = int(input(f'Enter number of {item_description}: '))
+# 4.
+item_price = float(input(f'Enter price per {item_description}: '))
+# 5.
+total_sales = item_number * item_price
+# 6.
+print(f'\n \nYour total order for {item_number} {item_description} is ${total_sales} at ${item_price} each.')
