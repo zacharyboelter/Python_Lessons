896633ecf61ebd9f60e48be062b6873f8059fddc
Zachary Boelter <zboelter@gmail.com>
2026-02-17 13:15:05 -0800

    final touches for assignment

--- Week_03/Project_03.py
+++ Week_03/Project_03.py
@@ -10,9 +10,9 @@ for i in range(num_of_employees):
     hours = float(input(f'Enter hours worked for employee {i+1}: '))
     rate = float(input(f'Enter hourly pay rate for employee {i+1}: '))
     #4.3
-    if hours < 40:
+    if hours <= 40:
         paycheck = hours * rate
     else:
         paycheck = (40 * rate) + ((hours - 40) * (rate * 1.5))
     #4.4
-    print(f'Employee worked {hours} hours for a gross wage of ${paycheck}')
+    print(f'Employee worked {hours} hours at ${rate} rate for a gross wage of ${paycheck}')
